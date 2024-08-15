ffdea995902b3c3a4084c3f504368b02fb747da1
ptyoiy <56474564+ptyoiy@users.noreply.github.com>
2024-08-16 03:24:23 +0900

    chapter: 9

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from "../../ui/skeletons";
+
+export default function Loading() {
+  // Or a custom loading skeleton component
+  return <DashboardSkeleton />
+}
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,5 @@
-export default function invoices() {
+export default async function invoices() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   return <h1>Welcome to invoices!</h1>;
 }
